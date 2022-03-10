363478dc325b8fc042ee56adbf334e188b576d1e
vance.petitt <vance.petitt@gmail.com>
2022-03-10 13:29:21 -0600

    Created song class

---
+++ songs/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.forms import CharField
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField()
---
+++ songs/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.song_list),
+    path('int<pk>/', views.song_detail),
+]
