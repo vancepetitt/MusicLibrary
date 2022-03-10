0e8c6d371313dcc7c53cf3278af03b8ef89ce2c6
vance.petitt <vance.petitt@gmail.com>
2022-03-10 16:55:32 -0600

    Created and tested the Get, put and delete fucntionality for Get_songs_by_ID, Update_Song, and Delete songs, and to have them return correct statuses.

--- songs/urls.py
+++ songs/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.songs_list),
-   # path('int<pk>/', views.song_detail),
+    path('<int:pk>/', views.song_detail),
 ]
--- songs/views.py
+++ songs/views.py
@@ -20,3 +20,18 @@ def songs_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
