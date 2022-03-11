8f53b5047924355e1400cba949b5da69cbcfeda6
vance.petitt <vance.petitt@gmail.com>
2022-03-11 10:35:49 -0600

    Deleted the Likes app to work in like logic at a later stage.

--- likes/models.py
+++
@@ -1,6 +0,0 @@
-import numbers
-from django.db import models
-
-# Create your models here.
-class LikeCounter(models.Model):
-    like_count = models.IntegerField()
--- likes/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . import models
-from .models import LikeCounter
-
-class LikeCounterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = LikeCounter
-        fields = ['like_count']
